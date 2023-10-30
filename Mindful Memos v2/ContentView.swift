80ae5e054942d1861e6c82a3c04e382026fc00f8
Brianna Magtoto <briannamagtoto@gmail.com>
2023-10-29 17:23:43 -0700

    Initial Commit

---
+++ Mindful Memos v2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mindful Memos v2
+//
+//  Created by Brianna Magtoto on 10/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mindful Memos v2/Mindful_Memos_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Mindful_Memos_v2App.swift
+//  Mindful Memos v2
+//
+//  Created by Brianna Magtoto on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Mindful_Memos_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
